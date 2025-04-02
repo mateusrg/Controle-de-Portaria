b1eb1ff38a7ab5acd3c8efda78214f2a7041a2fe
murilo1903 <murilofb1983@gmail.com>
2025-04-02 08:31:31 -0300

    Condomínio

--- client/js/script.js
+++ client/js/script.js
@@ -5,3 +5,15 @@ function mostrarSection(bloco) {
     section.style.display = "block";
     titulo.textContent = `Apartamentos Bloco ${bloco}`;
 }
+
+function mostrarModal(apto) {
+    const modal = document.getElementById("modal");
+    const titulo2 = document.getElementById("titulo-moradores");
+    modal.style.display = "none";
+    modal.style.display = "block";
+    titulo2.textContent = `Moradores ${apto}`;
+}
+
+closeModal.onclick = function() {
+    modal.style.display = "none";
+}
